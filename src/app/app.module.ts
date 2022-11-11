b1fa03754a11d4a74149f210a7dd91b8e544e2a8
Maki CAMARA <camara.maki@gmail.com>
2022-11-11 20:05:26 +0100

    ajout de modules routage et ajout d un mat card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule, Routes, Router} from '@angular/router'; //pour definir les routages
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { CarteComponent } from './carte/carte.component';
+import { HomeComponent } from './home/home.component';
+import { ReserveComponent } from './reserve/reserve.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes=[ //le ': Routes' permet de typer
+  {path: '', component: HomeComponent},
+  {path: 'carte', component: CarteComponent},
+  {path: 'reserve', component:ReserveComponent},
+  {path:'contact', component:ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    CarteComponent,
+    HomeComponent,
+    ReserveComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    RouterModule.forRoot(routes) //à ajouter pour naviguer entre les page, donc pour completer le routage
   ],
   providers: [],
   bootstrap: [AppComponent]

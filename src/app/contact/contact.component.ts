dbd5428810c2d1db2a085287ddc08339844430c0
Maki CAMARA <camara.maki@gmail.com>
2022-11-24 08:39:01 +0100

    Comm between components:the input decorator

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  standard={
+    firstName:'Maeva',
+    lastName:'Fess',
+    role: 'Glouton',
+    phoneNumber:'0680780759'
+  }
+  somelier={
+    firstName:'Maki',
+    lastName:'Cam',
+    role: 'Accueil',
+    phoneNumber:'0680780779'
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/phone/phone.component.ts
+++ src/app/phone/phone.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-phone',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phone.component.css']
 })
 export class PhoneComponent implements OnInit {
-
+@Input()
+user:any;
   constructor() { }
 
   ngOnInit(): void {
